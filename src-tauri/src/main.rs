01a52c9979c5f995d6f0bdc1da899f615000465e
FCSRobotics <mschoifet@friendscentral.org>
2023-10-11 03:20:57 +0900

    changes incomplete

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -78,21 +78,32 @@ impl AppState {
 struct Index(usize);
 
 
-#[derive(Clone,serde::Deserialize, serde::Serialize)]
+#[derive(Clone,serde::Deserialize, serde::Serialize, Default)]
 struct Team {
 
     number: String,
 }
 
 
-#[derive(serde::Deserialize, serde::Serialize)]
+#[derive(serde::Deserialize, serde::Serialize, Default)]
 struct TeamOverview {
     wins: usize,
     losses: usize,
-    lifetime_cones: usize,
-    lifetime_cubes: usize,
+    lifetime_auto_grid: usize,
+    lifetime_auto_balance: usize,
+    lifetime_mobility: usize,
+    lifetime_teleop_grid: usize,
+    lifetime_balance: usize,
+    lifetime_parked: usize,
+    lifetime_auto_pieces: usize,
+    lifetime_teleop_pieces: usize,
+    lifetime_auto_rank: usize,
+    lifetime_balance_sucesses: usize,
+    lifetime_balance_attempts: usize,
+    lifetime_overall_rank: usize,
+    lifetime_cycle_len: f64,
     team: Team,
-    average_pieces: f64, 
+ 
 }
 
 #[derive(Clone,serde::Deserialize, serde::Serialize)]
@@ -112,6 +123,7 @@ struct TeamGameData {
     mid_cubes: usize,
     low_cubes: usize,
     defense: bool,
+
     balanced: usize,
     parked: bool,
     catastrophic_failure: bool,
@@ -159,11 +171,52 @@ fn calculate_overview(data: Vec<TeamGameData>) -> TeamOverview {
     let mut overview = TeamOverview { wins: 0, losses: 0, lifetime_cones: 0, lifetime_cubes: 0, team: data[0].team.clone() , average_pieces: 0.0};
     for game in data.clone() {
         if game.won {overview.wins += 1;} else {overview.losses +=1}
-        overview.lifetime_cones += game.high_cones as usize;
-        overview.lifetime_cubes += game.high_cubes as usize;
-        
+
+        let auto_grid = 6*(game.high_cones_auto+game.high_cubes_auto) + 4*(game.mid_cones_auto+game.mid_cubes_auto) + 3*(game.low_cones_auto+game.low_cubes_auto);
+        overview.lifetime_auto_grid += auto_grid;
+        let mut auto_balance_points: usize = 0;
+        if game.balanced_auto==1 {let auto_balance_points=12;} else if (game.balanced_auto==3) {let auto_balance_points=8;}
+        overview.lifetime_auto_balance+=auto_balance_points;
+        if game.mobility_auto {overview.lifetime_mobility+=1;}
+        let teleop_grid=5*(game.high_cones+game.high_cubes) + 3*(game.mid_cones+game.mid_cubes) + 2*(game.low_cones+game.low_cubes);
+        overview.lifetime_teleop_grid += teleop_grid;
+        let mut balance: usize = 0;
+        if game.balanced==1 {balance+=10} else if game.balanced==3 {balance+=6}
+        overview.lifetime_balance+=balance;
+        if game.parked {overview.lifetime_parked+=2;}
+        let points=(auto_grid+auto_balance_points+teleop_grid+balance);
+        overview.lifetime_auto_pieces += game.high_cones_auto+game.high_cubes_auto + game.mid_cones_auto+game.mid_cubes_auto + game.low_cones_auto + game.low_cubes_auto;
+        overview.lifetime_auto_rank += (auto_grid+auto_balance_points);
+        if game.balanced_auto==1 {overview.lifetime_auto_rank +=5;} else if (game.balanced_auto==3) {overview.lifetime_auto_rank -=3;} else if (game.balanced_auto==0) {overview.lifetime_auto_rank -=5}
+        if game.mobility_auto {overview.lifetime_auto_rank+=2}
+        if game.balanced_auto==1 || game.balanced_auto==3 || game.balanced_auto==0 {overview.lifetime_balance_attempts+=1}
+        if game.balanced_auto==1 {overview.lifetime_balance_sucesses+=1}
+        let tele_pieces = game.high_cones+game.high_cubes + game.mid_cones+game.mid_cubes + game.low_cones+game.low_cubes;
+        overview.lifetime_teleop_pieces += tele_pieces;
+        if game.balanced==1 || game.balanced==3 || game.balanced==0 {overview.lifetime_cycle_len += 115.0/(tele_pieces as f64);} else {overview.lifetime_cycle_len += 135.0/(tele_pieces as f64)}
+        let mut overall_rank = (auto_grid+auto_balance_points+teleop_grid+balance);
+        if game.balanced_auto==1 {overall_rank +=5;} else if game.balanced_auto==3 {overall_rank -=3;} else if game.balanced_auto==0 {overall_rank -=5}
+        if game.mobility_auto {overall_rank+=2}
+        if game.balanced==1 {overall_rank +=2;} else if game.balanced==3 {overall_rank -=5;} else if game.balanced==0 {overall_rank -=10}
+        if game.parked {overall_rank +=2}
+        if game.sabotage {overall_rank -=10}
+        if game.catastrophic_failure {overall_rank -=20}
+        if game.won {overall_rank +=10}
+        overall_rank += game.drive_grade;//driving_score;
+        overall_rank += game.overall_grade;
+        if game.defense {overall_rank +=5}
+        overall_rank += tele_pieces*3;
+        overview.lifetime_overall_rank += overall_rank;
     }
-    overview.average_pieces = ((overview.lifetime_cones + overview.lifetime_cubes) as f64) / (data.len() as f64);
+
+    overview.average_cycle=lifetime_cycle_len/data.len();
+    overview.average_auto=overview.lifetime_auto_grid+overview.lifetime_auto_balance/ data.len();
+    overview.average_teleop=lifetime_teleop_grid+lifetime_teleop_balance+lifetime_parked/ data.len();
+    overview.balance_skill=lifetime_balance_sucesses/lifetime_balance_attempts;
+    overview.average_auto_rank=lifetime_auto_rank/data.len();
+    overview.average_teleop_peices = lifetime_teleop_peices/data.len();
+    average_rank=lifetime_overall_rank/data.len();
+    average_score=average_teleop+avearage_auto;
     return overview;
 }
 
